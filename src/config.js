3e9bd05fd3c5fce1b00a25d94a0d8a0dbe66f347
OceaniaVPN <odimokiy228@gmail.com>
2026-09-10 17:28:55 +0300

    Add VPN configuration module

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export function getConfig(env) {
+  return {
+    telegramToken: env.TELEGRAM_BOT_TOKEN,
+    githubToken: env.GITHUB_TOKEN,
+    adminId: Number(env.ADMIN_ID || 0),
+    configRepoOwner: env.CONFIG_REPO_OWNER || "OceaniaVPN",
+    configRepoName: env.CONFIG_REPO_NAME || "StekloVPN",
+    configsFolder: env.CONFIGS_FOLDER || "configs",
+    branch: env.BRANCH || "main",
+    workerOrigin: (env.WORKER_ORIGIN || "").replace(/\/$/, ""),
+    subscriptionSecret: env.SUBSCRIPTION_SECRET || env.TELEGRAM_BOT_TOKEN || "",
+    webhookSecret: env.TELEGRAM_WEBHOOK_SECRET || "",
+    db: env.DB || null,
+  };
+}
+
+export function escapeHtml(value) {
+  return String(value ?? "").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+export function isValidChatId(value) { return /^-?\d{1,20}$/.test(String(value || "")); }
+export function isSafeConfigFilename(value) { return /^(?:user_-?\d{1,20}|decoded_-?\d{1,20}_[a-z0-9]+)\.txt$/i.test(String(value || "")); }
